08bbdc4cb9c679b652b0d01cfbccd3ae8bcb9157
Micha Sengotta <micha.sengotta@gmail.com>
2022-01-05 23:37:14 +0100

    Add an "all" package to the home screen

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:gestures/models/app_content.dart';
+import 'package:gestures/models/package.dart';
 import 'package:gestures/screens/home/components/package_list_tile.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -9,10 +10,18 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final allGestures = [...appContent.packages.expand((p) => p.gestures)];
+    allGestures.sort((g1, g2) => g1.title.compareTo(g2.title));
+    final combinedPackage = Package(
+      title: 'Alle',
+      gestures: allGestures,
+    );
+
     return Scaffold(
       appBar: AppBar(title: const Text('Ursberger Gebärden - Paket wählen')),
       body: ListView(
         children: [
+          PackageListTile(package: combinedPackage),
           for (final package in appContent.packages)
             PackageListTile(package: package),
         ],
