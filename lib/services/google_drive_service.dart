3a6420373220efe869da12745cd1488a60c6b339
Micha Sengotta <micha.sengotta@gmail.com>
2022-01-14 11:01:13 +0100

    Fix google drive sharing link regex

--- lib/services/google_drive_service.dart
+++ lib/services/google_drive_service.dart
@@ -1,16 +1,12 @@
 class GoogleDriveService {
   static final RegExp sharingLinkPattern =
-      RegExp('https://drive\\.google\\.com/file/d/([\\w\\n]+)/.*');
+      RegExp('https://drive\\.google\\.com/file/d/([\\w\\n-]+)/.*');
 
   final String apiKey;
 
   GoogleDriveService({required this.apiKey});
 
   String? toDirectLink(String sharingLink) {
-    // AIzaSyBVjAU58Izq4tLrpUCxbqyyt2iOFroJc88
-    // https://drive.google.com/file/d/1aQZT7PNf_GI3XdeAK7rBFaU_KFRH46mF/view?usp=sharing;
-    // https://www.googleapis.com/drive/v3/files/1aQZT7PNf_GI3XdeAK7rBFaU_KFRH46mF?alt=media&key=AIzaSyBVjAU58Izq4tLrpUCxbqyyt2iOFroJc88
-
     final match = sharingLinkPattern.matchAsPrefix(sharingLink);
     if (match == null) return null;
 
