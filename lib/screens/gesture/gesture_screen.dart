d01778a0774c87e197fddd8899e95ba72541bfba
Micha Sengotta <micha.sengotta@gmail.com>
2023-04-28 20:20:59 +0200

    Add jumping between first and last gesture

--- lib/screens/gesture/gesture_screen.dart
+++ lib/screens/gesture/gesture_screen.dart
@@ -20,25 +20,23 @@ class _GestureScreenState extends State<GestureScreen> {
   late final PageController _controller;
   final _transitionDuration = const Duration(milliseconds: 500);
   final _transitionCurve = Curves.easeInOut;
+  Future<void> _pageTransition = Future.value();
 
   @override
   void initState() {
-    _controller = PageController(initialPage: widget.initialIndex);
     super.initState();
+    _controller = PageController(initialPage: widget.initialIndex);
   }
 
-  void _previous() {
-    _controller.previousPage(
-      duration: _transitionDuration,
-      curve: _transitionCurve,
-    );
-  }
-
-  void _next() {
-    _controller.nextPage(
-      duration: _transitionDuration,
-      curve: _transitionCurve,
-    );
+  void _animateToPage(int index) {
+    final loopingIndex = index % widget.gestures.length;
+    setState(() {
+      _pageTransition = _controller.animateToPage(
+        loopingIndex,
+        duration: _transitionDuration,
+        curve: _transitionCurve,
+      );
+    });
   }
 
   @override
@@ -49,10 +47,18 @@ class _GestureScreenState extends State<GestureScreen> {
         controller: _controller,
         itemCount: widget.gestures.length,
         itemBuilder: (context, index) {
-          return GestureView(
-            gesture: widget.gestures[index],
-            onPrevious: (index == 0) ? null : _previous,
-            onNext: (index == widget.gestures.length - 1) ? null : _next,
+          return FutureBuilder(
+            future: _pageTransition,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState != ConnectionState.done)
+                return Container(); // prevent playback during page transition
+
+              return GestureView(
+                gesture: widget.gestures[index],
+                onPrevious: () => _animateToPage(index - 1),
+                onNext: () => _animateToPage(index + 1),
+              );
+            },
           );
         },
       ),
