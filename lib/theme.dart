9d05b92eb690bfba7ec303c919ce749775f6da0e
Micha Sengotta <micha.sengotta@gmail.com>
2023-08-27 22:47:24 +0200

    Make theme colors private to prevent outside use

--- lib/theme.dart
+++ lib/theme.dart
@@ -2,31 +2,31 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 // DRW corporate design
-final primaryColor = Color(0xffa60430);
-final accentColor = Color(0xffedd7d5);
-// final surfaceColor = Color(0xfff0f0ed);
-// final surfaceColor2 = Color(0xffe1e1dd);
-const backgroundColor = Colors.white;
+final _primaryColor = Color(0xffa60430);
+final _accentColor = Color(0xffedd7d5);
+// final _surfaceColor = Color(0xfff0f0ed);
+// final _surfaceColor2 = Color(0xffe1e1dd);
+const _backgroundColor = Colors.white;
 
 // not defined in corporate design
-const onLightColor = Color(0xff4d4d4d);
-const onDarkColor = Colors.white;
+const _onLightColor = Color(0xff4d4d4d);
+const _onDarkColor = Colors.white;
 
 final theme = ThemeData.from(
   textTheme: GoogleFonts.firaSansTextTheme(),
   colorScheme: ColorScheme(
     brightness: Brightness.light,
-    primary: primaryColor,
-    onPrimary: onDarkColor,
-    secondary: accentColor,
-    onSecondary: onLightColor,
-    error: primaryColor,
-    onError: onDarkColor,
-    background: backgroundColor,
-    onBackground: onLightColor,
+    primary: _primaryColor,
+    onPrimary: _onDarkColor,
+    secondary: _accentColor,
+    onSecondary: _onLightColor,
+    error: _primaryColor,
+    onError: _onDarkColor,
+    background: _backgroundColor,
+    onBackground: _onLightColor,
     // using backgroundColor as surfaceColor, because of the BG image
-    surface: backgroundColor.withAlpha(216),
-    onSurface: onLightColor,
+    surface: _backgroundColor.withAlpha(216),
+    onSurface: _onLightColor,
   ),
 ).copyWith(
   inputDecorationTheme: InputDecorationTheme(
@@ -36,15 +36,15 @@ final theme = ThemeData.from(
   appBarTheme: AppBarTheme(
     centerTitle: true,
     toolbarHeight: 64,
-    backgroundColor: primaryColor,
-    foregroundColor: onDarkColor,
+    backgroundColor: _primaryColor,
+    foregroundColor: _onDarkColor,
     shape: ContinuousRectangleBorder(
       borderRadius: BorderRadius.vertical(
         bottom: Radius.circular(50),
       ),
     ),
     titleTextStyle: TextStyle(
-      color: onDarkColor,
+      color: _onDarkColor,
     ),
   ),
 );
