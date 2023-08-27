1dc44ae63e6e8260a37db5d49428eed4df8e2e9e
Micha Sengotta <micha.sengotta@gmail.com>
2023-08-27 22:46:17 +0200

    Convert App to a stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -23,16 +23,9 @@ void main() async {
   runApp(const App());
 }
 
-class App extends StatefulWidget {
-  const App({
-    super.key,
-  });
+class App extends StatelessWidget {
+  const App({super.key});
 
-  @override
-  State<App> createState() => _AppState();
-}
-
-class _AppState extends State<App> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
